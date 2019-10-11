3804cdd23a9ea976727448738f9e80fb52da48f7
ANDREYDEN <andrey2850@gmail.com>
2019-10-11 01:28:25 -0400

    Added basic layout
    - created `SearchResults` and `ConstellationSearch`
    - imported components and fixed styling

---
+++ src/components/compound/SearchResults.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ConstellationSearch></ConstellationSearch>
+        <ConstellationSearch></ConstellationSearch>
+        <ConstellationSearch></ConstellationSearch>
+    </div>
+</template>
+
+<script>
+import ConstellationSearch from '../shared/ConstellationSearch.vue'
+export default {
+	components: {
+		ConstellationSearch,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/shared/ConstellationSearch.vue
@@ -0,0 +1,50 @@
+<template>
+    <div v-bind:class="[{relevant:relevant}, irrelevant]" class="search-result">
+        <div class="card-circle"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        relevant: {
+            type: Boolean,
+            default: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import '../../styles/colors';
+
+    .search-result {
+        width: 50%;
+        height: 90px;
+        margin: 0;
+        padding-top: 10px;
+        border-radius: 20px;
+        position: relative;
+    }
+
+    .relevant {
+        background-image: gradient(to bottom, #071238, #05081a);
+    }
+
+    .irrelevant {
+        background-image: gradient(to bottom, white, rgb(173, 173, 173));
+    }
+
+    .card-circle {
+        right: 9px;
+        position: absolute;
+        border: 4px solid white;
+        border-radius: 50px;
+        width: 50px;
+        height: 50px;
+    }
+
+    .blured {
+        filter: blur(4px);
+    }
+</style>
