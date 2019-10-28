9ee9b1114ab43ba8627ffb0a2e1b75d23486839b
karen <gallegokaren1897@gmail.com>
2019-10-27 20:02:21 -0400

    Add infinite scrolling animation

--- src/components/compound/ColoredRow.vue
+++ src/components/compound/ColoredRow.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>
-        <MarqueeCard label="Education" color-scheme="teal"></MarqueeCard>
-        <MarqueeCard label="Careers" color-scheme="blue"></MarqueeCard>
-        <MarqueeCard label="Wellbeing" color-scheme="grey"></MarqueeCard>
-        <MarqueeCard label="Design" color-scheme="grey"></MarqueeCard>
-        <MarqueeCard label="Health" color-scheme="grey"></MarqueeCard>
+    <div class="container">
+        <div class="row">
+            <MarqueeCard card="card" label="Education" color-scheme="teal"></MarqueeCard>
+            <MarqueeCard card="card" label="Careers" color-scheme="blue"></MarqueeCard>
+            <MarqueeCard card="card" label="Wellbeing" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Design" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Health" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Finances" color-scheme="grey"></MarqueeCard>
+        </div>
+        <div class="row">
+            <MarqueeCard card="card" label="Education" color-scheme="teal"></MarqueeCard>
+            <MarqueeCard card="card" label="Careers" color-scheme="blue"></MarqueeCard>
+            <MarqueeCard card="card" label="Wellbeing" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Design" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Health" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Finances" color-scheme="grey"></MarqueeCard>
+        </div>
+        <div class="row">
+            <MarqueeCard card="card" label="Education" color-scheme="teal"></MarqueeCard>
+            <MarqueeCard card="card" label="Careers" color-scheme="blue"></MarqueeCard>
+            <MarqueeCard card="card" label="Wellbeing" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Design" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Health" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard card="card" label="Finances" color-scheme="grey"></MarqueeCard>
+        </div>
     </div>
 </template>
 
@@ -19,8 +38,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    div {
+    .container {
         display: flex;
         flex-wrap: nowrap;
     }
+
+    .row {
+        animation: marquee 25s linear infinite;
+        overflow: hidden;
+        display: flex;
+    }
+
+    @keyframes marquee {
+        0% {
+            transform: translateX(0);
+        }
+        100% {
+            transform: translateX(100%);
+        }
+    }
+
 </style>
--- src/components/compound/GreyRow.vue
+++ src/components/compound/GreyRow.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>
-        <MarqueeCard label="Education" color-scheme="grey"></MarqueeCard>
-        <MarqueeCard label="Careers" color-scheme="grey"></MarqueeCard>
-        <MarqueeCard label="Wellbeing" color-scheme="grey"></MarqueeCard>
-        <MarqueeCard label="Design" color-scheme="grey"></MarqueeCard>
-        <MarqueeCard label="Health" color-scheme="grey"></MarqueeCard>
+    <div class="container">
+        <div class="row">
+            <MarqueeCard label="Education" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Careers" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Wellbeing" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Design" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Health" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Finances" color-scheme="grey"></MarqueeCard>
+        </div>
+        <div class="row">
+            <MarqueeCard label="Education" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Careers" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Wellbeing" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Design" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Health" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Finances" color-scheme="grey"></MarqueeCard>
+        </div>
+        <div class="row">
+            <MarqueeCard label="Education" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Careers" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Wellbeing" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Design" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Health" color-scheme="grey"></MarqueeCard>
+            <MarqueeCard label="Finances" color-scheme="grey"></MarqueeCard>
+        </div>
     </div>
 </template>
 
@@ -19,8 +38,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    div {
+
+    .container {
         display: flex;
         flex-wrap: nowrap;
     }
+
+    .row {
+        animation: marquee 30s linear infinite;
+        overflow: hidden;
+        display: flex;
+    }
+
+    @keyframes marquee {
+        0% {
+            transform: translateX(0);
+        }
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+
 </style>
